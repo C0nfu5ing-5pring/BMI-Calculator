351b699a29cb7dc18607157091cf09918db58d8d
C0nfu5ing_5pring <shishfruitwala29@gmail.com>
2023-10-10 20:34:07 +0530

    second-commit

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+
+
+
 $(".check-BMI").click(function () {
     let userName = $(".name-input").val();
     let userWeight = $(".weight-input").val();
@@ -27,7 +30,22 @@ $(".check-BMI").click(function () {
         $(".output-2").text("Obese Class III");
     }
 });
+$(".dark-bg-button").click(function () {
+    $("body").attr("class", "dark");
+    $(".hr").attr("class", "dark-hr")
+    $(".card").attr("class", "dark-card")
+    $(".name").attr("class", "dark-name")
+    $(".name").attr("class", "dark-name")
+    $(".height").attr("class", "dark-height")
+    $(".weight").attr("class", "dark-weight ");
+    $(".check-BMI").attr("class", "check-BMI-dark");
+    $(".reload").attr("class", "dark-reload");
+    $(".copyright").attr("class", "copyright-dark");
+    $(".output").attr("class", "output-dark ");
+    $(".output-2").attr("class", "output-2-dark ");
+});
 
 $(".reload").click(function () {
     location.reload();
 });
+
